734b437c0aff3202085b4b7f403bc8884dd0f5b3
keenm <treefrogapplications@gmail.com>
2021-02-13 19:51:35 +0000

    add Flow extensions

---
+++ coroutines/src/test/kotlin/com/treefrogapps/kotlin/coroutines/flow/extensions/Flow.kt
@@ -0,0 +1,16 @@
+package com.treefrogapps.kotlin.coroutines.flow.extensions
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.take
+
+
+fun <T> Flow<T>.first(): Flow<T> = take(1)
+
+fun <T> Flow<T>.firstIf(block: (T) -> Boolean): Flow<T> = take(1).filter { block(it) }
+
+fun <T> Flow<T>.firstOrDefault(default: T, block: (T) -> Boolean): Flow<T> =
+    take(1)
+        .filter { block(it) }
+        .onEmpty { emit(default) }
