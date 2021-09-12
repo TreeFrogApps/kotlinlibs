7a793adedcc28028ad54df756ffc16f1f727703a
keenm <treefrogapplications@gmail.com>
2021-09-12 22:00:13 +0100

    bump version 1.5.5 bad for some reason ...

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 group = "com.treefrogapps.kotlin.coroutines"
 project.extra["name"] = "coroutines"
-version = "1.5.5"
+version = "1.5.6"
 
 dependencies {
     // Kotlin
