1875bab7c7baa3e247e03f2f6231d6547276459e
keenm <treefrogapplications@gmail.com>
2023-01-04 21:56:31 +0000

    update junit dep - todo - move to version catalogs in settings gradle file, or toml file

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -11,6 +11,6 @@ dependencies {
     // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-    testImplementation("junit:junit:4.13.1")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
