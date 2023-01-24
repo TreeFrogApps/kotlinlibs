4a58f8f22a4f0582513212b57945dc35c66c61df
keenm <treefrogapplications@gmail.com>
2023-01-24 21:40:19 +0000

    add utility string function for returning char offirst Char second String if different or null

--- core/src/main/kotlin/com/treefrogapps/kotlin/core/extensions/String.kt
+++ core/src/main/kotlin/com/treefrogapps/kotlin/core/extensions/String.kt
@@ -2,3 +2,11 @@ package com.treefrogapps.kotlin.core.extensions
 
 
 fun emptyString() : String = ""
+
+fun firstCharIfDifferent(
+    string: String,
+    string2: String
+): Char? =
+    string2.firstOrNull()
+        ?.uppercaseChar()
+        .let { char -> if (char != string.firstOrNull()?.uppercaseChar()) char else null }
