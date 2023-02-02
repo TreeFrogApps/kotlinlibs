15e56527ad901041fac70e420306b74140e381a8
keenm <treefrogapplications@gmail.com>
2023-02-02 20:15:54 +0000

    update to kotlin 1.8.0.  core : remove result class - prefer to use standard result class which removed return type restrictions.  Add onComplete extension for Result class

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 group = "com.treefrogapps.kotlin.core"
 project.extra["name"] = "core"
-version = "1.13.1"
+version = "1.14.0"
 
 dependencies {
     // Kotlin
--- core/src/main/kotlin/com/treefrogapps/kotlin/core/extensions/Result.kt
+++ core/src/main/kotlin/com/treefrogapps/kotlin/core/extensions/Result.kt
@@ -1,73 +1,13 @@
 package com.treefrogapps.kotlin.core.extensions
 
-class Result<out T> private constructor(private val value: T? = null, private val throwable: Throwable? = null) {
 
-    companion object {
-
-        fun <T> success(t: T) = Result<T>(value = t)
-        fun <T, E : Throwable> failure(e: E) = Result<T>(throwable = e)
-    }
-
-    val isSuccess: Boolean get() = value != null
-    val isFailure: Boolean get() = throwable != null
-
-    fun getOrNull(): T? = value
-
-    fun exceptionOrNull(): Throwable? = throwable
-
-    fun <E : Throwable> getOrThrow(e: E): T =
-            when {
-                isSuccess -> value!!
-                else      -> throw e
-            }
-
-    override fun equals(other: Any?): Boolean =
-            when {
-                isSuccess -> value!! == other
-                else      -> throwable!! == other
-            }
-
-    override fun hashCode(): Int =
-            when {
-                isSuccess -> value!!.hashCode()
-                else      -> throwable!!.hashCode()
-            }
-
-    override fun toString(): String =
-            when {
-                isSuccess -> value!!.toString()
-                else      -> throwable!!.toString()
-            }
+inline fun <T> Result<T>.onComplete(action : () -> Unit): Result<T> {
+    action()
+    return this
 }
 
-inline fun <T, R> Result<T>.flatMap(func: (T) -> Result<R>): Result<R> =
-        when {
-            isSuccess -> func(getOrNull()!!)
-            else      -> Result.failure(exceptionOrNull()!!)
-        }
-
-inline fun <T, R> Result<T>.map(func: (T) -> R): Result<R> =
-        when {
-            isSuccess -> Result.success(func(getOrNull()!!))
-            else      -> Result.failure(exceptionOrNull()!!)
-        }
-
-inline fun <T> Result<T>.orElseGet(func: () -> T): T =
-        when {
-            isSuccess -> getOrNull()!!
-            else      -> func()
-        }
-
-fun <T> Result<T>.orElse(t: T): T =
-        when {
-            isSuccess -> getOrNull()!!
-            else      -> t
-        }
-
-inline fun <T> Result<T>.filter(func: (T) -> Boolean): Result<T> =
-        when {
-            isSuccess && func(getOrNull()!!) -> Result.success(getOrNull()!!)
-            else                             -> Result.failure(exceptionOrNull()!!)
-        }
-
 fun <T> Result<T>.getOrDefault(value: T): T = getOrNull() ?: value
+
+fun et() {
+    Result.success(1).onSuccess {  }
+}
