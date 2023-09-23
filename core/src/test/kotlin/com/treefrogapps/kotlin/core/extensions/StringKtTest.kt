8f18609fcd1da2a1ddf83a261e414d1f97ec16dd
keenm <treefrogapplications@gmail.com>
2023-09-23 13:37:18 +0100

    upversion kotlin bump lib versions

---
+++ core/src/test/kotlin/com/treefrogapps/kotlin/core/extensions/StringKtTest.kt
@@ -0,0 +1,64 @@
+package com.treefrogapps.kotlin.core.extensions
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class StringKtTest {
+
+    @Test
+    fun `When first string empty and same as string2, Then null result`() {
+        // When
+        val string = emptyString()
+
+        val actual = firstCharIfDifferent(string = string, string2 = string)
+        // Then
+        assertNull(actual)
+    }
+
+    @Test
+    fun `When first string not empty and same as string2, Then null result`() {
+        // When
+        val string = "T"
+
+        val actual = firstCharIfDifferent(string = string, string2 = string)
+        // Then
+        assertNull(actual)
+    }
+
+    @Test
+    fun `When first string not empty and string2 uppercase, Then null result`() {
+        // When
+        val string = "t"
+        val string2 = "T"
+
+        val actual = firstCharIfDifferent(string = string, string2 = string2)
+        
+        // Then
+        assertNull(actual)
+    }
+
+    @Test
+    fun `When first string empty and string2 not empty, Then string2 first char result`() {
+        // When
+        val string = ""
+        val string2 = "A"
+        val expected = string2.first()
+
+        val actual = firstCharIfDifferent(string = string, string2 = string2)
+        // Then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `When first string empty and string2 lowercase and not empty, Then string2 first char uppercase result`() {
+        // When
+        val string = ""
+        val string2 = "a"
+        val expected = string2.first().uppercaseChar()
+
+        val actual = firstCharIfDifferent(string = string, string2 = string2)
+        // Then
+        assertEquals(expected, actual)
+    }
+}
--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 group = "com.treefrogapps.kotlin.coroutines"
 project.extra["name"] = "coroutines"
-version = "2.3.0"
+version = "2.4.0"
 
 dependencies {
     // Kotlin
