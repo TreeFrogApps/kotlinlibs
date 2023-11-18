2b40db477a2a05aa38d31e2c0fce374555d9ddc6
keenm <treefrogapplications@gmail.com>
2023-11-18 14:50:35 +0000

    bump kotlin version

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -2,15 +2,15 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 group = "com.treefrogapps.kotlin.coroutines"
 project.extra["name"] = "coroutines"
-version = "2.4.0"
+version = "2.5.0"
 
 dependencies {
     // Kotlin
     implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
 
     // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
