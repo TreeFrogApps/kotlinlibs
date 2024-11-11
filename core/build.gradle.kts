4c6be8ccd094048ead291b83196e184f5becbca8
keenm <treefrogapplications@gmail.com>
2024-11-11 22:18:46 +0000

    bump kotlin version. Bump core and coroutines

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,6 @@
 group = "com.treefrogapps.kotlin.core"
 project.extra["name"] = "core"
-version = "1.20.1"
+version = "1.20.2"
 
 dependencies {
     // Kotlin
--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -1,6 +1,6 @@
 group = "com.treefrogapps.kotlin.coroutines"
 project.extra["name"] = "coroutines"
-version = "2.5.4"
+version = "2.5.5"
 
 dependencies {
     // Kotlin
